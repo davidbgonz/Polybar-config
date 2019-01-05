6faa80e088a055b3810dedcbb33d4cec7910522e
David <david.gonz789@gmail.com>
2019-01-05 12:24:38 -0500

    Launch appropriate polybars for dual and three monitor setups

---
+++ launch-polybar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+DESKTOP=$(echo $DESKTOP_SESSION)
+
+case $DESKTOP in
+	i3)
+	if type "xrandr" > /dev/null; then
+		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+			if [ $m == 'eDP-1' ]; then
+				export MONITOR=$m
+				polybar --reload center -c ~/.config/polybar/config &
+			fi
+			if [ $m == 'HDMI-1' -o $m == 'DVI-I-1-1' ]; then
+				export MONITOR_RIGHT=$m
+				polybar --reload right -c ~/.config/polybar/config &
+			fi
+			if [ $m == 'DVI-I-2-2' ]; then
+				export MONITOR_LEFT=$m
+				polybar --reload left -c ~/.config/polybar/config &
+			fi
+		done
+	fi
+	;;
+esac

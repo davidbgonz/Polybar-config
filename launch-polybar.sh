dcbecb908516bde5fb05af7f1af4ecb3b1789ea7
David <david.gonz789@gmail.com>
2019-03-10 13:30:12 -0400

    Adjusted screen setup so on dual monitor HDMI-1 gets app tray

--- launch-polybar.sh
+++ launch-polybar.sh
@@ -15,12 +15,12 @@ case $DESKTOP in
 				export MONITOR=$m
 				polybar --reload center -c ~/.config/polybar/config &
 			fi
-			if [ $m == 'DVI-I-1-1' ]; then
-				export MONITOR_LEFT=$m
+			if [ $m == 'DVI-I-1-1' -o $m == 'HDMI-1' ]; then
+				export EXT_MONITOR_LEFT=$m
 				polybar --reload left -c ~/.config/polybar/config &
 			fi
-			if [ $m == 'HDMI-1' -o $m == 'DVI-I-2-2' ]; then
-				export MONITOR_RIGHT=$m
+			if [ $m == 'DVI-I-2-2' ]; then
+				export EXT_MONITOR_RIGHT=$m
 				polybar --reload right -c ~/.config/polybar/config &
 			fi
 		done

bc14473efac901108fce7c439e1098a8aeef5be2
David Gonzalez <dgonzalez@tagonline.com>
2019-03-28 13:04:57 -0400

    Made polybar tray dynamic depending on monitor setup.
    If only one monitor is set (only laptop), then the center polybar will
    have the tray. If either my two or three monitor setup is active, the
    polybar left will have the tray.

--- launch-polybar.sh
+++ launch-polybar.sh
@@ -12,18 +12,27 @@ case $DESKTOP in
 	if type "xrandr" > /dev/null; then
 		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 			if [ $m == 'eDP-1' ]; then
-				export MONITOR=$m
-				polybar --reload center -c ~/.config/polybar/config &
-			fi
-			if [ $m == 'DVI-I-1-1' -o $m == 'HDMI-1' ]; then
-				export EXT_MONITOR_LEFT=$m
-				polybar --reload left -c ~/.config/polybar/config &
-			fi
-			if [ $m == 'DVI-I-2-2' ]; then
-				export EXT_MONITOR_RIGHT=$m
-				polybar --reload right -c ~/.config/polybar/config &
+				export MONITOR="$m"
+				export TRAY_POS_MAIN="right"
+			elif [ $m == 'DVI-I-1-1' -o $m == 'HDMI-1' ]; then
+				export EXT_MONITOR_LEFT="$m"
+			elif [ $m == 'DVI-I-2-2' ]; then
+				export EXT_MONITOR_RIGHT="$m"
+				# Sets left polybar (currently EXT_MONITOR_LEFT) with tray and unset's laptop montitor tray position
+				export TRAY_POS_ALT="right"
+				unset TRAY_POS_MAIN
 			fi
 		done
 	fi
 	;;
 esac
+
+if [ -n "$MONITOR" ]; then
+	polybar --reload center -c ~/.config/polybar/config &
+fi
+if [ -n "$EXT_MONITOR_LEFT" ]; then
+	polybar --reload left -c ~/.config/polybar/config &
+fi
+if [ -n "$EXT_MONITOR_RIGHT" ]; then
+	polybar --reload right -c ~/.config/polybar/config &
+fi

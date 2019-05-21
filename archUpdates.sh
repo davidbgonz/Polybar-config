7b5056d6b5fc206adaab99bd0c6d14697bbdb30a
David <david.gonz789@gmail.com>
2019-05-21 15:28:41 -0400

    Query updates in yay only for aur
    Whenever the arch package repositories are synced, it causes yay to show
    updates for all packages not including aur. Setting an aur argument
    forces yay to check for updates only for aur.

--- archUpdates.sh
+++ archUpdates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 updates_arch=$(checkupdates 2> /dev/null | wc -l)
-updates_aur=$(yay -Qu 2> /dev/null | wc -l)
+updates_aur=$(yay -Qu --aur 2> /dev/null | wc -l)
 
 if [ -z $updates_arch ]; then
     updates_arch=0

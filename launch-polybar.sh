18a75e233bf91c135481cb4d3202eff568facc63
David <david.gonz789@gmail.com>
2021-01-10 17:17:24 -0500

    Updated polybar launch for new display

--- launch-polybar.sh
+++ launch-polybar.sh
@@ -10,38 +10,42 @@ DESKTOP=$(echo $DESKTOP_SESSION)
 RIGHT_MODULES_MAIN=""
 
 case $DESKTOP in
-	i3)
-	if type "xrandr" > /dev/null; then
-		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-			if [ $m == 'eDP-1' -o $m == 'DP-1-3' ]; then
-				export MONITOR="$m"
+    i3)
+    if type "xrandr" > /dev/null; then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+            if [ $m == 'eDP-1' -o $m == 'DP-1-3' ]; then
+                export MONITOR="$m"
 
-				if [ $m == 'eDP-1' ]; then
-					export TRAY_POS_MAIN="right"
-					export RIGHT_MODULES_MAIN="weather backlight-acpi alsa battery date powermenu"
-				else
-					unset TRAY_POS_MAIN
-					export RIGHT_MODULES_MAIN="weather backlight-acpi eth wlan alsa battery date powermenu"
-				fi
-			elif [ $m == 'DP-1-1' -o $m == 'DVI-I-1-1' -o $m == 'HDMI-1' ]; then
-				export EXT_MONITOR_LEFT="$m"
-				export TRAY_POS_ALT="right"
-				export RIGHT_MODULES_MAIN="weather backlight-acpi eth wlan alsa battery date powermenu"
-				unset TRAY_POS_MAIN
-			elif [ $m == 'DP-1-2' -o $m == 'DVI-I-2-2' ]; then
-				export EXT_MONITOR_RIGHT="$m"
-			fi
-		done
-	fi
-	;;
+                if [ $m == 'eDP-1' ]; then
+                    export TRAY_POS_MAIN="right"
+                    export BORDER_RIGHT_SIZE_MAIN=5
+                    export RIGHT_MODULES_MAIN="weather backlight-acpi alsa battery date powermenu"
+                else
+                    unset TRAY_POS_MAIN
+                    unset BORDER_RIGHT_SIZE_MAIN
+                    export RIGHT_MODULES_MAIN="weather backlight-acpi eth wlan alsa battery date powermenu"
+                fi
+            elif [ $m == 'DP-1' -o $m == 'DP-1-1' -o $m == 'DVI-I-1-1' -o $m == 'HDMI-1' ]; then
+                export EXT_MONITOR_LEFT="$m"
+                export TRAY_POS_ALT="right"
+                export BORDER_RIGHT_SIZE_ALT=5
+                export RIGHT_MODULES_MAIN="weather backlight-acpi eth wlan alsa battery date powermenu"
+                unset TRAY_POS_MAIN
+                unset BORDER_RIGHT_SIZE_MAIN
+            elif [ $m == 'DP-1-2' -o $m == 'DVI-I-2-2' ]; then
+                export EXT_MONITOR_RIGHT="$m"
+            fi
+        done
+    fi
+    ;;
 esac
 
 if [ -n "$MONITOR" ]; then
-	polybar --reload center -c ~/.config/polybar/config 2>/var/log/polybar/polybar-center.log &
+    polybar --reload center -c ~/.config/polybar/config 2>/var/log/polybar/polybar-center.log &
 fi
 if [ -n "$EXT_MONITOR_LEFT" ]; then
-	polybar --reload left -c ~/.config/polybar/config 2>/var/log/polybar/polybar-left.log &
+    polybar --reload left -c ~/.config/polybar/config 2>/var/log/polybar/polybar-left.log &
 fi
 if [ -n "$EXT_MONITOR_RIGHT" ]; then
-	polybar --reload right -c ~/.config/polybar/config 2>/var/log/polybar/polybar-right.log &
+    polybar --reload right -c ~/.config/polybar/config 2>/var/log/polybar/polybar-right.log &
 fi

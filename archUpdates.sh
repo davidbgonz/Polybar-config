1a13efd78049c05c6a36374fa206771e86755098
David <david.gonz789@gmail.com>
2021-03-13 14:58:19 -0500

    Clean up arch update script

--- archUpdates.sh
+++ archUpdates.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 updates_arch=$(checkupdates 2> /dev/null | wc -l)
-#updates_arch=$(pacman -Sup 2> /dev/null | wc -l)
 updates_aur=$(yay -Qu --aur 2> /dev/null | wc -l)
 
 if [ -z $updates_arch ]; then
@@ -16,7 +15,6 @@ updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates" -gt 0 ]; then
     echo "%{A1:pamac-manager --updates:} $updates_arch  $updates_aur%{A}"
-    #echo " $updates_arch  $updates_aur"
     #echo " $updates_arch  $updates_aur"
 else
     echo ""
